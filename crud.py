768cc8a3d311be69dbb9897f81a1760e75a820bf
maxim <andreev_m@garpix.com>
2022-03-31 09:15:34 +0300

    init commit2

--- crud.py
+++ crud.py
@@ -4,15 +4,12 @@ import models, schemas
 
 
 def get_user(db: Session, user_id: int):
-<<<<<<< HEAD
-<<<<<<< HEAD
     user = db.query(models.User).filter(models.User.id == user_id)
-=======
+
     user = db.query(models.User).filter(models.User.id == user_id).first()
->>>>>>> 9204f15... init commit
-=======
+
     user = db.query(models.User).filter(models.User.id == user_id)
->>>>>>> 3cd5876... init commit
+
     return user
 
 
@@ -22,14 +19,7 @@ def get_users(db: Session):
 
 def create_user(db: Session, request: schemas.UserCreate):
     fake_hashed_password = '123qweewq312'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    print(request.dict())
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
-    user = models.User(email=request.email, hashed_password=fake_hashed_password)
+    user = models.User(**request.dict(), hashed_password=fake_hashed_password)
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -40,10 +30,6 @@ def delete_user(user_id: int, db: Session):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     db.delete(user)
     db.commit()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3cd5876... init commit
     return user
 
 
@@ -54,16 +40,6 @@ def update_user(db: Session, request: schemas.UserUpdate, user):
 
 def get_item(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id)
-<<<<<<< HEAD
-=======
-    return {'result': 'success'}
-
-
-def get_item(db: Session, item_id: int):
-    return db.query(models.Item).filter(models.User.id == item_id).first()
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
 
 
 def get_items(db: Session):
@@ -76,10 +52,6 @@ def create_item(db: Session, request: schemas.ItemCreate, owner_id: int):
     db.commit()
     db.refresh(item)
     return item
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3cd5876... init commit
 
 
 def update_item(db: Session, request: schemas.Item, item):
@@ -91,8 +63,3 @@ def delete_item(item, db: Session):
     db.delete(item)
     db.commit()
     return {'deleted'}
-<<<<<<< HEAD
-=======
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
--- main.py
+++ main.py
@@ -1,70 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3cd5876... init commit
+from routing import item, user
+
 from fastapi import FastAPI
+
 from models import Base
 from database import engine
-from routing import item, user
-<<<<<<< HEAD
-=======
-from typing import Optional, List
-from pydantic import BaseModel
-from fastapi import FastAPI, Depends, HTTPException
-import datetime
-import crud, models
-from models import Base
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from schemas import UserCreate, User
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.include_router(user.router)
 app.include_router(item.router)
-=======
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post('/user/', response_model=User)
-def create_user(request: UserCreate, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == request.email).first()
-    if user:
-        raise HTTPException(status_code=400, detail='Email already registered')
-    return crud.create_user(db, request)
-
-
-@app.get('/user/', response_model=List[User])
-def get_users(db: Session = Depends(get_db)):
-    return crud.get_users(db)
-
-
-@app.get('/user/{user_id}', response_model=User)
-def retrieve_user(user_id: int, db: Session = Depends(get_db)):
-    user = crud.get_user(db, user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail='User not Found')
-    return user
-
-
-@app.delete('/user/{user_id}')
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    return crud.delete_user(user_id, db)
->>>>>>> 9204f15... init commit
-=======
-app.include_router(user.router)
-app.include_router(item.router)
->>>>>>> 3cd5876... init commit
