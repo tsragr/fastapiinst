5d31e4c3eb4f4e1544c2da481a75411f866433af
maxim <andreev_m@garpix.com>
2022-03-31 10:08:34 +0300

    init commit2

--- schemas.py
+++ schemas.py
@@ -3,10 +3,6 @@ import datetime
 from pydantic import BaseModel
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3cd5876... init commit
 class UserBase(BaseModel):
     email: str
 
@@ -21,16 +17,13 @@ class ItemBase(BaseModel):
 
 class ItemUpdate(ItemBase):
     pass
-<<<<<<< HEAD
-=======
+
+
 class ItemBase(BaseModel):
     title: str
     description: Optional[str] = None
     time_created: datetime.datetime
     time_updated: datetime.datetime = None
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
 
 
 class ItemCreate(ItemBase):
@@ -39,36 +32,17 @@ class ItemCreate(ItemBase):
 
 class Item(ItemBase):
     id: int
-<<<<<<< HEAD
-<<<<<<< HEAD
-    owner: UserBase
-    time_created: datetime.datetime
-    time_updated: datetime.datetime = None
-=======
+
     owner_id: int
->>>>>>> 9204f15... init commit
-=======
-    owner: UserBase
-    time_created: datetime.datetime
-    time_updated: datetime.datetime = None
->>>>>>> 3cd5876... init commit
 
     class Config:
         orm_mode = True
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3cd5876... init commit
-class UserCreate(UserBase):
-    password: str
-
-
 class UserUpdate(UserBase):
     pass
-<<<<<<< HEAD
-=======
+
+
 class UserBase(BaseModel):
     email: str
 
@@ -76,9 +50,6 @@ class UserBase(BaseModel):
 class UserCreate(BaseModel):
     email: str
     password: str
->>>>>>> 9204f15... init commit
-=======
->>>>>>> 3cd5876... init commit
 
 
 class User(UserBase):
